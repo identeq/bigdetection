c52f74ac5a8933ebe93d6fb1b154d459c0acbfaa
Pravesh22 <budhathokipravesh5@gmail.com>
2024-04-01 17:06:51 +0545

    example updated

--- example/image_demo.py
+++ example/image_demo.py
@@ -5,20 +5,21 @@
 """
 import time
 
-from big_detection.mmdet.apis.inference import init_detector, inference_detector, show_result_pyplot
+from big_detection.mmdet.apis.inference import init_detector, inference_detector, show_result_pyplot, init_detector_v1
 
 
 def image_od_demo():
     # build the model from a config and a checkpoint file
-    config = "/home/dell/Documents/git_repos/bigdetection/big_detection/configs/BigDetection/cbnetv2/htc_cbv2_swin_base_giou_4conv1f_adamw_bigdet.py"
-    checkpoint = "htc_cbv2_swin_base_giou_4conv1f_bigdet.pth"
+    # config = "/home/dell/Documents/git_repos/bigdetection/big_detection/configs/BigDetection/cbnetv2/htc_cbv2_swin_base_giou_4conv1f_adamw_bigdet.py"
+    checkpoint = "/home/dell/Documents/git_repos/bigdetection_model/htc_cbv2_swin_base_giou_4conv1f_bigdet.pth"
     device = "cpu"
-    img = "/home/dell/Documents/git_repos/big_detection_test_result/demo_input/demo4.png"
+    img = "/home/dell/Documents/git_repos/big_detection_test_result/demo_input/demo5.png"
     score_thr = 0.3
-    out_file = "/home/dell/Documents/demo4_output.jpg"
+    out_file = "/home/dell/Documents/demo5_output.jpg"
     start_time = time.time()
     print("Start time: ", start_time)
-    model = init_detector(config, checkpoint, device=device)
+    # model = init_detector_v1(config, checkpoint, device=device)
+    model = init_detector_v1(checkpoint=checkpoint, device=device)
     # test a single image
     result = inference_detector(model, img)
     print(f"Total time taken: {time.time() - start_time}")
